a355209ff26295dbe9212317c9ad501614f8f71f
druliwho <druliwho@gmail.com>
2026-05-31 14:42:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 pacman -S --noconfirm xorg-server lightdm lightdm-gtk-greeter openbox picom dmenu
 systemctl enable lightdm.service
 
 cp -f autostart /etc/xdg/openbox/autostart
 cp -f rc.xml /etc/xdg/openbox/rc.xml
 cp -f lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
+
+cd /
+rm -rf "$SCRIPT_DIR"
